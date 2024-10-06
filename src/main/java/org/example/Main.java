03dbb01623fe819ef01ce64c78e0c494d203323b
GraceTenney <graceatenney@gmail.com>
2024-10-06 13:53:57 -0400

    midterm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,16 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        java.util.concurrent.TimeUnit.SECONDS.sleep(30);
         System.out.println("Hello world!");
-        /*JOptionPane.showMessageDialog(null, "Hello World");
-        int option = JOptionPane.showConfirmDialog(null, "Do the thing");
-        checkThing(option);
+        //JOptionPane.showMessageDialog(null, "Hello World");
+        //int option = JOptionPane.showConfirmDialog(null, "Do the thing");
+        int option = (int) (Math.random() * 3);
+        System.out.println(option + " " + checkThing(option));
         // -1 = exit
         // 0 = yes
         // 1 = no
         // 2 = cancel
-        JOptionPane.showMessageDialog(null, option + "" + checkThing(option));*/
+        //JOptionPane.showMessageDialog(null, option + "" + checkThing(option));
     }
     public static boolean checkThing(int option) {
         return option == 0;
